85d0afd3d2590f969175bc07555a10334a61d32a
david galloway <david.galloway@yahoo.co.uk>
2020-08-24 10:38:10 +0100

    master - added default get to return the last added element of the ...  value type using apache LinkedMap

--- src/main/kotlin/TypeSafeMapMap.kt
+++ src/main/kotlin/TypeSafeMapMap.kt
@@ -1,32 +1,39 @@
+import org.apache.commons.collections4.map.LinkedMap
 import java.lang.reflect.Type
 
 interface ITypeSafeMapMap {
     /*TODO: - can we remove this map property and still make the get work? */
     val map: LinkedHashMap<Type, Any>
-    fun <K: Any,V: Any>put(key: K, value: V)
+    fun <K : Any, V : Any> put(key: K, value: V)
     /* TODO: implement the rest of MutableMap<K,V> (Or even non generic version) interface methods where possible? */
 }
 
-inline fun <K:Any, reified V:Any> ITypeSafeMapMap.get(key: K): V {
-    return (map[V::class.java] as LinkedHashMap<*, *>).get(key = key)as V
+inline fun <K : Any, reified V : Any> ITypeSafeMapMap.get(key: K): V {
+    val linkedMap = map[V::class.java] as LinkedMap<*, *>
+    return linkedMap.get(key = key) as V
+}
+
+inline fun <reified V : Any> ITypeSafeMapMap.get(): V {
+    val linkedMap = map[V::class.java] as LinkedMap<*, *>
+    return linkedMap[linkedMap.lastKey()] as V
 }
 
 class TypeSafeMapMap : ITypeSafeMapMap {
     /*TODO: parameterise the underlying map implementation  */
     override val map: LinkedHashMap<Type, Any> = LinkedHashMap()
 
-    override fun <K:Any,V:Any>put(key: K, value: V){
-        if(!map.containsKey(key = value::class.java)){
-            map[value::class.java] = LinkedHashMap<Any,V>()
+    override fun <K : Any, V : Any> put(key: K, value: V) {
+        if (!map.containsKey(key = value::class.java)) {
+            // todo: follow apache's advice for concurrency and thread safety and wrap in a synchronized map
+            map[value::class.java] = LinkedMap<Any, V>() //LinkedHashMap<Any,V>()
         }
-        /*TODO:- this is the big one, can we removed this unchecked cast and still make this class work?*/
+        /*TODO:- this is the big one, can we remove this unchecked cast and still make this class work?*/
         @Suppress("UNCHECKED_CAST")
-        (map[value::class.java] as LinkedHashMap<Any,V>)[key] = value
+        (map[value::class.java] as LinkedMap<Any, V>)[key] = value
     }
-// inline reified methods can't be defined on an interface, but can be defined as extensions.
-    // this isn't the same thing, but allows us to use an abstraction instead of having to use the concrete
-    // class
-//    inline fun <K:Any, reified V:Any>get(key: K) : V{
-//        return (map[V::class.java] as HashMap<Any,V>).get(key = key)as V
-//    }
+    // inline reified methods can't be defined on an interface, but can be defined as extensions.
+    // this isn't the same thing, but allows us to use an abstraction instead of having to use the concrete class
+    //    inline fun <K:Any, reified V:Any>get(key: K) : V{
+    //        return (map[V::class.java] as HashMap<Any,V>).get(key = key)as V
+    //    }
 }
--- src/test/kotlin/TypeSafeMapMapTest.kt
+++ src/test/kotlin/TypeSafeMapMapTest.kt
@@ -24,30 +24,58 @@ class TypeSafeMapMapTest {
     }
 
     @Test
-    fun setStringValue(){
+    fun setStringValue() {
         val valToSave = "Hello, World!"
         val keyToSave = "greeting"
-        typeSafeMapMap.put(key = keyToSave,value = valToSave)
-        val retrieved:String = typeSafeMapMap.get(keyToSave)
+        typeSafeMapMap.put(key = keyToSave, value = valToSave)
+        val retrieved: String = typeSafeMapMap.get(keyToSave)
         assertThat(retrieved).isEqualTo(valToSave)
 
     }
 
     @Test
-    fun setIntValue(){
+    fun setIntValue() {
         val valToSave = 1
         val keyToSave = "one"
-        typeSafeMapMap.put(key = keyToSave,value = valToSave)
-        val retrieved:Int = typeSafeMapMap.get(keyToSave)
+        typeSafeMapMap.put(key = keyToSave, value = valToSave)
+        val retrieved: Int = typeSafeMapMap.get(keyToSave)
         assertThat(retrieved).isEqualTo(valToSave)
     }
 
     @Test
-    fun setCustomClassValue(){
+    fun setIntValueWithIntKey() {
+        val valToSave = 1
+        val keyToSave = 1
+        typeSafeMapMap.put(key = keyToSave, value = valToSave)
+        val retrieved: Int = typeSafeMapMap.get(keyToSave)
+        assertThat(retrieved).isEqualTo(valToSave)
+    }
+
+    @Test
+    fun setIntValueWithIntKeySameInstance() {
+        val kvToSave = 1
+        typeSafeMapMap.put(key = kvToSave, value = kvToSave)
+        val retrieved: Int = typeSafeMapMap.get(kvToSave)
+        assertThat(retrieved).isEqualTo(kvToSave)
+    }
+
+    @Test
+    fun setCustomClassValue() {
         val valToSave = this
         val keyToSave = "TypeSafeMapMapTest"
-        typeSafeMapMap.put(key = keyToSave,value = valToSave)
-        val retrieved:TypeSafeMapMapTest = typeSafeMapMap.get(keyToSave)
+        typeSafeMapMap.put(key = keyToSave, value = valToSave)
+        val retrieved: TypeSafeMapMapTest = typeSafeMapMap.get(keyToSave)
         assertThat(retrieved).isEqualTo(valToSave)
+    }
+
+    @Test
+    fun retrieveLastValueByDefault() {
+        for (x in 1..10) {
+            typeSafeMapMap.put(key = x, value = x)
+            val retrieved: Int = typeSafeMapMap.get(x)
+            assertThat(retrieved).isEqualTo(x)
+        }
+        val lastInt: Int = typeSafeMapMap.get()
+        assertThat(lastInt).isEqualTo(10)
     }
 }
